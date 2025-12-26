f4fb17265344821739b66648fae7964b00705979
krupa-1926 <patelkrupa1926@gmail.com>
2025-12-26 20:22:57 +0530

    mongoDb update

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectWithDB = () => {
   mongoose.set('strictQuery', false);
-  mongoose
-    .connect(process.env.DB_URL, {
+  mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
